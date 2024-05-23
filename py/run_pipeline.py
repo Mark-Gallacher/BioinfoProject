cb0ce11c4e66967856cd94ed5d58bee65e2c4efc
Mark-Gallacher <106823536+Mark-Gallacher@users.noreply.github.com>
2024-05-23 13:59:11 +0100

    altering the hyperparameters for KNN to focus on the peak more

--- py/run_pipeline.py
+++ py/run_pipeline.py
@@ -94,7 +94,7 @@ knn_params = Hyperparametres(
             model_name = "KNearestNeighbours", 
             model_code = "KNN", 
             params = {
-            "n_neighbors" : [2, 3, 4, 5, 6, 8, 10, 12, 16, 20, 24, 28, 32, 36, 40], 
+            "n_neighbors" : [2, 4, 6, 8, 10, 11, 12, 13, 14, 15, 17, 20, 25, 30, 35, 40], 
             "weights" : ["uniform", "distance"]
             })
 
