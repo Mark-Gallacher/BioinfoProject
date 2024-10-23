5b1d28f128517c1eb23812f48c945570a1772be5
Mark-Gallacher <106823536+Mark-Gallacher@users.noreply.github.com>
2024-10-23 16:39:46 +0100

    fixing name and input validation

--- py/HelperFunctions.py
+++ py/HelperFunctions.py
@@ -1,103 +1,36 @@
-from sklearn.metrics import make_scorer, recall_score, precision_score, f1_score, fbeta_score, roc_auc_score, cohen_kappa_score, matthews_corrcoef, log_loss
-# import numpy as np
+def check_type(arg_value, arg_name: str, exp_type) -> None:
+    """Function to check the argument value (`arg_value`) is the expected type (`exp_type`).
+    Generates an Error message and raises a TypeError.
+    """
+    ## check if we expect one type or multiple
+    if isinstance(exp_type, list):
 
-def generate_scorers(metric:str) -> dict:
+        ## check if the type is one of the expected ones
+        if not any(isinstance(arg_value, e) for e in exp_type):
 
-    metric_scorers = {"f1" : f1_score,
-                      "fbeta": fbeta_score,
-                      "precision": precision_score, 
-                      "recall" : recall_score, 
-                      "roc_auc" : roc_auc_score}
+            raise TypeError(
+                f"""Error with `{arg_name}` not the expected type:
+                  - Expected: {[e.__name__ + " , " for e in exp_type]} 
+                  - Received: {arg_value} ({type(arg_value).__name__})"""
+            )
 
-    metrics_suffix = ["macro", "micro", "weighted"]
-
-    ## ensure the metric string is in our dictionary
-    if metric not in metric_scorers.keys():
+        ## If arg is one of the expected types
         return None
-    
-    ## fbeta requires additional arguments - handle separately
-    if metric == "fbeta":
-        betas = [2, 3, 4]
 
-        ## generate a dictionary of all the variations of the scoring functions
-        scorer = {metric + "_" + str(beta) + "_" + average : 
-                  make_scorer(
-                      metric_scorers[metric],
-                      beta = beta,
-                      average = average, 
-                      zero_division = 0, 
-                      # response_method = "predict",
-                      labels = ['CS', 'HV', 'PA', 'PHT', 'PPGL']
-                      )
-
-                  for average in metrics_suffix
-                  for beta in betas}
-
-        return scorer
-
-    if metric == "roc_auc":
-        ## generate a dictionary of all the variations of the scoring functions
-        scorer = {metric + "_" + average : 
-                  make_scorer(
-                      metric_scorers[metric],
-                      multi_class = "ovr",  ## One vs the Rest - default is raise.
-                      average = average, 
-                      # response_method = "predict",
-                      labels = ['CS', 'HV', 'PA', 'PHT', 'PPGL']
-                      )
-
-                  for average in metrics_suffix}
-
-        return scorer
-
-    ## generate a dictionary of all the variations of the scoring functions
-    scorer = {metric + "_" + average : make_scorer(
-                                       metric_scorers[metric], 
-                                       # response_method = "predict",
-                                       average = average, 
-                                       zero_division = 0,
-                                       labels = ['CS', 'HV', 'PA', 'PHT', 'PPGL'], 
-                                       pos_label = None
-                                       )
-              for average in metrics_suffix}
-
-    return scorer
-
-# print(generate_scorers("fbeta"))
+    ## if arg is expected to be just one type
+    if not isinstance(arg_value, exp_type):
+        raise TypeError(
+            f"""Error with `{arg_name}` not the expected type:
+              - Expected: {exp_type.__name__} 
+              - Received: {arg_value} ({type(arg_value).__name__})"""
+        )
 
 
-def expand_metrics(metrics:list) -> dict:
+def check_types(args: list) -> None:
+    """Function to iterate over a tuple containing (value, name, type) for an argument. Input validation for a function - see HelperFunctions.check_type() for order."""
+    for arg_value, arg_name, exp_type in args:
 
-    ## check the metrics supplied are a list and have more than one element in them
-    if not isinstance(metrics, list):
-        raise ValueError("metrics should be a list of strings")
+        check_type(arg_value, arg_name, exp_type)
 
-    if len(metrics) < 1:
-        raise ValueError("metrics should not be an empty list")
 
-    expandable_metrics = ["f1", "precision", "recall", "fbeta", "roc_auc"]
-
-    output = {}
-    for metric in metrics:
-        if metric not in expandable_metrics:
-
-            output[metric] = metric
-        
-        else:
-
-            ## generate all the scoring functions for each metric
-            expanded_metrics = generate_scorers(metric)
-
-            if expanded_metrics is None:
-                continue
-
-            ## then add to the dictionary
-            for new_metric in expanded_metrics.keys():
-                output[new_metric] = expanded_metrics[new_metric]
-
-    return output
-
-# assert expand_metrics(["hello"]) == {"hello" : "hello"}
-# assert expand_metrics(["f1"]) == {"f1_micro" : "f1_micro", 
-                                # "f1_macro" : "f1_macro", 
-                                # "f1_weighted" : "f1_weighted"}
+# check_types(("a", "b", [str, int]))
--- py/Model.py
+++ py/Model.py
@@ -1,42 +1,57 @@
-from sklearn.base import is_classifier 
+from sklearn.base import is_classifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import cross_validate
 from sklearn.model_selection import ParameterGrid
 
 import pandas as pd
+from HelperFunctions import check_types
+
 
 ## A class for the hyperparametres of a model, which is basically a dictionary
-## this allows it to integrate with the sci-kit API down the line but allows 
+## this allows it to integrate with the sci-kit API down the line but allows
 ## us to keep track of what parameters we could change
-class Hyperparameters():
-    """ `Hyperparameters` represents the hyperparameter grid for a model. 
+class Hyperparameters:
+    """`Hyperparameters` represents the hyperparameter grid for a model.
     Requires three arguments for initialisation:
         - `model_name` - a String to name the model - used to name created files
-        - `model_code` - a String for a short name for the model - used to create IDs for the individual combinations of hyperparameters. 
+        - `model_code` - a String for a short name for the model - used to create IDs for the individual combinations of hyperparameters.
         - `params` - a Dictionary (or List) containing the hyperparameters with the values, in the format:
-            - 'name_of_hyperparameter' : [x_1, x_2, ... , x_n] or 
+            - 'name_of_hyperparameter' : [x_1, x_2, ... , x_n] or
             - [ {'hyperparameter_1' : [x_1, x_2, ..., x_n]},
-            -   {'hyperparameter_2' : [y_1, y_2, ..., y_n]} ] or 
+            -   {'hyperparameter_2' : [y_1, y_2, ..., y_n]} ] or
             - {} ## if no hyperparameters are to be passed to model
 
     For Example:
-        - `model_name` could be 'LogisticRegression' with the `model_code` of 'LG'. 
-        - `params` could be {"C" : [0.1, 1, 10]} - to control the penalty term 'C'. 
+        - `model_name` could be 'LogisticRegression' with the `model_code` of 'LG'.
+        - `params` could be {"C" : [0.1, 1, 10]} - to control the penalty term 'C'.
     """
 
-    def __init__(self, model_name:str, model_code:str, params:dict )-> None:
-        
+    def __init__(self, model_name: str, model_code: str, params: dict) -> None:
+
+        ## validate the inputs are the correct type
+        check_types(
+            [
+                (model_name, "model_name", str),
+                (model_code, "model_code", str),
+                (params, "params", [list, dict]),
+            ]
+        )
+
         ## ensure a useful model name is supplied - defined by length
-        if len(model_name) < 3 :
-            raise ValueError(f"Please Supply a more useful name than {model_name}, for example - LogisticRegression")
+        if len(model_name) < 1:
+            raise ValueError(
+                f"Please Supply a more useful name than {model_name}, for example - LogisticRegression"
+            )
 
         ## ensure a useful model code is supplied - defined by length
-        if len(model_code) < 2 :
-            raise ValueError(f"Please supply a more useful code than {model_code}, for example - LG for Logistic Regression")
+        if len(model_code) < 1:
+            raise ValueError(
+                f"Please supply a more useful code than {model_code}, for example - LG for Logistic Regression"
+            )
 
         self.params = params
         self.model_code = model_code.upper()
-        self.model_name = model_name 
+        self.model_name = model_name
 
         ## process the params supplied - form the grid of all the combinations
         self.grid = self.create_grid()
@@ -44,9 +59,8 @@ class Hyperparameters():
         ## assign each unique model with a ID using the model_code
         self.param_ids = self.create_ids()
 
-
     def create_grid(self) -> list:
-        """ Returns a list of the unique combinations of all the hyperparameters. 
+        """Returns a list of the unique combinations of all the hyperparameters.
         Uses the ParameterGrid from Scikit-Learn.
         """
 
@@ -58,56 +72,60 @@ class Hyperparameters():
         ## if the dictionary is empty
         else:
 
-            grid = [{"NA" : "NA"}]
+            grid = [{"NA": "NA"}]
 
         return grid
 
     def create_ids(self) -> list:
-        """ Returns a list of the unique IDs. 
-        - Used to identify the individual combination of hyperparameter. 
+        """Returns a list of the unique IDs.
+        - Used to identify the individual combination of hyperparameter.
         - IDs have form `model_code`-number (if code is LG, then LG-1, LG-2, ..., LG-n)
         """
 
         ## check grid has a truthy value
         if self.grid is None:
-            raise AttributeError(f"Expected a dictionary, but got None instead, from grid attribute, received: {self.grid}")
-        
-        ## check that value has a length greater than one 
+            raise AttributeError(
+                f"Expected a dictionary, but got None instead, from grid attribute, received: {self.grid}"
+            )
+
+        ## check that value has a length greater than one
         if len(self.grid) < 1:
-            raise AttributeError("Expected a populated dictionary, but received an empty dictionary instead")
-        
-        
+            raise AttributeError(
+                "Expected a populated dictionary, but received an empty dictionary instead"
+            )
+
         num_params = range(1, len(self.grid) + 1)
 
-        ## IDs use model_code with a dash then a number. 
+        ## IDs use model_code with a dash then a number.
         return [self.model_code + "-" + str(num) for num in num_params]
-    
 
     def parse_param_dicts(self) -> dict:
-        """ Parses the Dictionary of hyperparametrics to a long format Dictionary. 
-        Returns a dictionary with three keys ('model_id', 'param' and 'value'). 
+        """Parses the Dictionary of hyperparametrics to a long format Dictionary.
+        Returns a dictionary with three keys ('model_id', 'param' and 'value').
         Used to generate a CSV file that can handle different number of hyperparameters.
         """
 
         if self.grid is None:
-            raise AttributeError("Expected a dictionary, but got None instead, from grid attribute")
-        
+            raise AttributeError(
+                "Expected a dictionary, but got None instead, from grid attribute"
+            )
+
         if self.param_ids is None:
-            raise AttributeError("Expected a dictionary, but got None instead, from grid attribute")
-        
+            raise AttributeError(
+                "Expected a dictionary, but got None instead, from grid attribute"
+            )
+
         ## pair up the unique IDs with the combinations of hyperparameters
         params_with_id = list(zip(self.param_ids, self.grid))
 
         ## define the output dictionary
-        parsed_dict = {"model_id" : [], 
-                       "param" : [], 
-                       "value" : []}
+        parsed_dict = {"model_id": [], "param": [], "value": []}
 
         ## iterate over the models
         for model in params_with_id:
 
             ## model is ["model_id", {dict of params} ]
-            ## some models can have several hyperparameters, all get parsed into the 
+            ## some models can have several hyperparameters, all get parsed into the
             ## same three 'columns'
             for param, value in model[1].items():
                 parsed_dict["model_id"].append(model[0])
@@ -115,19 +133,16 @@ class Hyperparameters():
                 parsed_dict["value"].append(value)
 
         return parsed_dict
-                
+
     def generate_params_dataframe(self) -> pd.DataFrame:
-        """ Simply converts the Dictionary of Hyperparameters into a Pandas.DataFrame. 
-        """
+        """Simply converts the Dictionary of Hyperparameters into a Pandas.DataFrame."""
 
         param_dict = self.parse_param_dicts()
 
         return pd.DataFrame(param_dict)
 
-
-    def save_as_csv(self, folder:str) -> None:
-        """ Saves a CSV file in `folder` with name: `model_name`.csv
-        """
+    def save_as_csv(self, folder: str) -> None:
+        """Saves a CSV file in `folder` with name: `model_name`.csv"""
 
         df = self.generate_params_dataframe()
 
@@ -136,8 +151,8 @@ class Hyperparameters():
         if folder.endswith("/"):
             folder = folder.rstrip("/")
 
-        try: 
-            df.to_csv(f"{folder}/{self.model_name}.csv", index = False)
+        try:
+            df.to_csv(f"{folder}/{self.model_name}.csv", index=False)
 
         except Exception as e:
 
@@ -148,25 +163,25 @@ class Hyperparameters():
 
 ## A class for the model, which contains the parameters to train the model across
 ## Some basic input validation will be nice but using a setter could make the __init__ method tidier
-## and allow for more complex validations or conditions. 
-class Model():
-    
-    def __init__(self, model, params:Hyperparameters, folds, n_jobs:int = 1,  **kwargs) -> None:
-        
+## and allow for more complex validations or conditions.
+class Model:
+
+    def __init__(
+        self, model, params: Hyperparameters, folds, n_jobs: int = 1, **kwargs
+    ) -> None:
+
         if not is_classifier(model):
             raise ValueError("model should be a classifer from sci-kit learn!")
 
-        self.model_name = params.model_name 
+        self.model_name = params.model_name
         self.code = params.model_code.upper()
         self.model = model(**kwargs)
-        self.params_grid = params 
+        self.params_grid = params
         self.trained_params = {}
         self.folds = folds
         self.cores = n_jobs
 
-
-
-    def cross_validate(self, X, y, metrics:dict) -> dict:
+    def cross_validate(self, X, y, metrics: dict) -> dict:
 
         ## this is true if the dictionary is not empty
         if self.params_grid.params:
@@ -174,58 +189,60 @@ class Model():
             try:
 
                 cv = GridSearchCV(
-                estimator = self.model,
-                param_grid = self.params_grid.params,
-                cv = self.folds,
-                scoring = metrics, 
-                refit = False, 
-                n_jobs = self.cores)
+                    estimator=self.model,
+                    param_grid=self.params_grid.params,
+                    cv=self.folds,
+                    scoring=metrics,
+                    refit=False,
+                    n_jobs=self.cores,
+                )
 
                 cv_ = cv.fit(X, y)
-           
+
                 ## extract the params used in the cv
                 self.trained_params = cv_.cv_results_["params"]
 
                 ## return a dictionary of the metric results - so it can be parsed
                 return cv_.cv_results_
-            
+
             except Exception as e:
-                print(f"Issue when running GridSearchCV!!\nModel: {self.model_name} \nError: {e}")            
+                print(
+                    f"Issue when running GridSearchCV!!\nModel: {self.model_name} \nError: {e}"
+                )
                 raise SystemExit(1)
-
-
 
         ## if the params dictionary is empty - no need for GridSearch
         else:
-            
+
             try:
 
-                cv_:dict = cross_validate(
-                        estimator = self.model, 
-                        X = X, 
-                        y = y, 
-                        cv = self.folds, 
-                        scoring = metrics, 
-                        n_jobs = self.cores)
+                cv_: dict = cross_validate(
+                    estimator=self.model,
+                    X=X,
+                    y=y,
+                    cv=self.folds,
+                    scoring=metrics,
+                    n_jobs=self.cores,
+                )
 
                 ## return a dictionary of the metric results - so it can be parsed
                 return cv_
 
             except Exception as e:
-                print(f"Issue when running cross_validate!!\nModel: {self.model_name} \nError: {e}")            
+                print(
+                    f"Issue when running cross_validate!!\nModel: {self.model_name} \nError: {e}"
+                )
                 raise SystemExit(1)
 
-
-
     def generate_ids(self):
-        """ Generates IDs for the individual folds for each model. 
-        The ID has the format "CODE-MODEL-FOLD", where Model and Fold 
+        """Generates IDs for the individual folds for each model.
+        The ID has the format "CODE-MODEL-FOLD", where Model and Fold
         are 1-based indices. This allows us to connect the params to the metrics.
         """
 
         if isinstance(self.folds, int):
             folds = self.folds
-        
+
         else:
             folds = self.folds.n_splits
 
@@ -246,8 +263,3 @@ class Model():
                     model_id.append(self.code + "-" + str(model) + "-" + str(fold))
 
         return model_id
-
-
-
-
-
